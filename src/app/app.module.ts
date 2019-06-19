41ce4abace984024582c60eb125e15dd22481e9c
Williampezz <Williamaugustopez@gmail.com>
2019-06-18 23:07:49 -0300

    LoginFuncional

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [CoreModule, AppRoutingModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDQeNhLWTfte-DO8XpNTB6vBj_GxKwOZ2c',
+    authDomain: 'ionic-firestore-taskscurso.firebaseapp.com',
+    databaseURL: 'https://ionic-firestore-taskscurso.firebaseio.com',
+    projectId: 'ionic-firestore-taskscurso',
+    storageBucket: 'ionic-firestore-taskscurso.appspot.com',
+    messagingSenderId: '948239653712',
+    appId: '1:948239653712:web:4519f293b8f86db3'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
